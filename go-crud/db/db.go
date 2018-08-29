b025fad8bacfb2267a3885730cd96a6eebfb7af8
Supun Dissanayake <supunlakshandissanayake@gmail.com>
2018-08-30 01:16:20 +0530

    added config for loading credentials

--- go-crud/db/db.go
+++ go-crud/db/db.go
@@ -2,17 +2,43 @@ package db
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql" //mysql driver
 )
 
+//config for server
+type serverConfig struct {
+	User         string `json:"mysql-user"`
+	Password     string `json:"mysql-password"`
+	DatabaseName string `json:"db-name"`
+	HostName     string `json:"host"`
+}
+
 //GetDatabase - returns a Database object
 func GetDatabase() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "developer:123@/XYZ_lakshan_dissanayake")
+	//setup configuration
+	config := loadConfiguration()
+	db, err := sql.Open("mysql", fmt.Sprintf("%v:%v@/%v", config.User, config.Password, config.DatabaseName))
 	if err == nil {
 		db.SetMaxIdleConns(10)
 		db.SetMaxOpenConns(20)
 		err = db.Ping()
 	}
 	return db, err
+}
+
+func loadConfiguration() serverConfig {
+	file := "./config.json"
+	var config serverConfig
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	return config
 }
